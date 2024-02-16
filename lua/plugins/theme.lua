9afa82eb7d479a8570078f162ee953e513141151
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-02-16 14:45:33 +0100

    Updating Config

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,9 @@
+return {
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
+  config = function()
+    require("onedarkpro").setup {
+      vim.cmd "colorscheme onedark_dark",
+    }
+  end,
+}
