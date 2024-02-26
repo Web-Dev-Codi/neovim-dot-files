95485b5382edba5c8dfe29061614914251a2d13d
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-02-26 22:06:27 +0100

    Stashing Config

--- lua/plugins/neoconf.lua
+++ lua/plugins/neoconf.lua
@@ -1,3 +1,10 @@
 return {
   "folke/neoconf.nvim",
+  priority = 1000,
+  lazy = false,
+  config = function()
+    require("neoconf").setup {
+      -- override any of the default settings here
+    }
+  end,
 }
--- lua/plugins/sidebar.lua
+++ lua/plugins/sidebar.lua
@@ -3,12 +3,26 @@ return {
     "sidebar-nvim/sidebar.nvim",
     main = "sidebar-nvim",
     cmd = { "SidebarNvimToggle", "SidebarNvimOpen" },
-    opts = {
-      open = true,
-      bindings = {
-        -- stylua: ignore
-        ['q'] = function() require('sidebar-nvim').close() end,
-      },
-    },
+    config = function()
+      require("sidebar-nvim").setup {
+        disable_default_keybindings = 0,
+        bindings = nil,
+        open = true,
+        side = "left",
+        initial_width = 35,
+        hide_statusline = false,
+        update_interval = 1000,
+        sections = { "datetime", "git", "diagnostics" },
+        section_separator = { "", "-----", "" },
+        section_title_separator = { "" },
+        containers = {
+          attach_shell = "/bin/sh",
+          show_all = true,
+          interval = 5000,
+        },
+        datetime = { format = "%a %b %d, %H:%M", clocks = { { name = "local" } } },
+        todos = { ignored_paths = { "~" } },
+      }
+    end,
   },
 }
