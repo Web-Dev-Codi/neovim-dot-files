943e0b29e1a12a5d1649bd8b3586ede1e139028d
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-03-15 09:56:03 +0100

    Reconfigured CMP, Might go back to coc.nvim

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -76,7 +76,7 @@ return {
         { name = "nvim_lsp", max_item_count = 20, group_index = 1 },
         { name = "nvim_lua", group_index = 1 },
         { name = "vim-dadbod-completion", group_index = 1 },
-        { name = "path", group_index = 2 },
+        { name = "path", group_index = 2, get_cwd = true, label_trailing_slash = true, trailing_slash = true },
         { name = "buffer", keyword_length = 2, max_item_count = 5, group_index = 2 },
       },
     }
