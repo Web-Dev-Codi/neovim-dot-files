e000754dc9bc2264034e11a36d8a5dcd8ded92e1
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-11-06 14:42:13 +0100

    Added snippets

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
 local opt = vim.opt
 
 opt.termguicolors = true
 
-opt.clipboard = "unnamedplus"
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
 
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -14,6 +21,12 @@ opt.softtabstop = 4
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
 
 -- Search
 opt.incsearch = true
@@ -25,6 +38,7 @@ opt.swapfile = false
 opt.virtualedit = "block"
 opt.inccommand = "split"
 opt.ignorecase = true
+opt.syntax = "on"
 
 vim.g.root_spec = { "cwd" }
 vim.g.omni_sql_no_default_maps = 1
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,4 +1,18 @@
 return {
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    "folke/lazydev.nvim",
+    ft = "lua",
+    lazy = true,
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true },
   { -- https://github.com/smjonas/inc-rename.nvim
     "smjonas/inc-rename.nvim",
     cmd = "IncRename",
