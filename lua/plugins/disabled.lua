145a2f749b86e05090036e0c1c75ca350199bbe0
Web-Dev-Codi <cordiscobrian@gmail.com>
2024-12-19 15:38:34 +0100

    Complete rewrite

--- lua/plugins/disabled.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Disable auto opening (){}[]
-  { "echasnovski/mini.pairs", enabled = true },
-  { "j-hui/fidget.nvim", enabled = true },
-  {
-    "garymjr/nvim-snippets",
-    enabled = true,
-  },
-}
